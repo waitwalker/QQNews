939ca30efe4ca7595af920918339fdf8375ae3d3
WaitWalker <waitwalker@163.com>
2017-06-14 22:38:40 +0800

    初始化项目,添加击类

---
+++ QQNews/QQNews/Common/BaseClass/ETTView.swift
@@ -0,0 +1,21 @@
+//
+//  ETTView.swift
+//  QQNews
+//
+//  Created by WangJunZi on 2017/6/14.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+import UIKit
+
+class ETTView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
