5a4eef9e121b49a7be8b1e5de0d0dd0caef0341e
WaitWalker <waitwalker@163.com>
2017-06-15 21:23:17 +0800

    添加模型文件

---
+++ QQNews/QQNews/Me(我)/Model/ETTMeModel.swift
@@ -0,0 +1,13 @@
+//
+//  ETTMeModel.swift
+//  QQNews
+//
+//  Created by WangJunZi on 2017/6/15.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+import UIKit
+
+class ETTMeModel: NSObject {
+
+}
